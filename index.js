ae4fc45c36f6b40d553b0c7a17dcf5abca4366d0
bjoda <bjorn.asplund@imbera.no>
2018-06-26 14:14:25 +0200

    work

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const PORT = process.env.PORT || 5001
 
 app.use(express.static(__dirname + '/client/dist/mb-shop'));
 
-// app.get('/', (req, res) => res.send('Hello Node and Heroku, Maridalen Brenneri is coming up!'))
+app.get('/hello', (req, res) => res.send('Hello Node and Heroku, Maridalen Brenneri is coming up!'))
 
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
