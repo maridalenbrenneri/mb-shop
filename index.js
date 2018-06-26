fdd542d2106c606898db381914af53a867da078b
bjoda <bjorn.asplund@imbera.no>
2018-06-26 14:00:04 +0200

    serving angular app from node

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 5001
 
-app.get('/', (req, res) => res.send('Hello Node and Heroku, Maridalen Brenneri is coming up!'))
+app.use(express.static('./client/dist/mb-shop'));
+
+// app.get('/', (req, res) => res.send('Hello Node and Heroku, Maridalen Brenneri is coming up!'))
 
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
