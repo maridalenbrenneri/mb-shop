0be42575c5ff80ea5f1bbd5290ed661a43b75cd9
bjoda <bjorn.asplund@imbera.no>
2018-06-26 14:35:07 +0200

    heroku config

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
-const express = require('express')
-const app = express()
-const PORT = process.env.PORT || 5001
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+// const forceSSL = function() {
+//     return function (req, res, next) {
+//         if (req.headers['x-forwarded-proto'] !== 'https') {
+//             return res.redirect(['https://', req.get('Host'), req.url].join(''));
+//         }
+//         next();
+//     }
+// }
+  
+// app.use(forceSSL());
 
 app.use(express.static(__dirname + '/client/dist/mb-shop'));
+
+app.get('/*', function(req, res) {
+    res.sendFile(__dirname + '/client/dist/mb-shop/index.html');
+});
 
 app.get('/hello', (req, res) => res.send('Hello Node and Heroku, Maridalen Brenneri is coming up!'))
 
