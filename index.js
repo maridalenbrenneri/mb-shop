f17d49bd0603b2dece84a67949f3f79b9d79bf83
bjoda <bjorn.asplund@imbera.no>
2018-06-26 14:11:54 +0200

    angular and node and heroku

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 5001
 
-app.use(express.static('./client/dist/mb-shop'));
+app.use(express.static(__dirname + '/client/dist/mb-shop'));
 
 // app.get('/', (req, res) => res.send('Hello Node and Heroku, Maridalen Brenneri is coming up!'))
 
