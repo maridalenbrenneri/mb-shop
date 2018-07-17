f187a254ba2bbbaefa4db50a12660754b3d90062
bjoda <bjorn.asplund@imbera.no>
2018-07-17 21:09:16 +0200

    Cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,41 +1,4 @@
-import { Component, Injectable } from '@angular/core';
-import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
-import { Observable } from 'rxjs';
-
-@Injectable()
-export class LoadingIndicatorInterceptor implements HttpInterceptor {
-  
-  constructor(private loadingIndicatorService: LoadingIndicatorService) {}
-  
-  intercept (req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    // emit onStarted event before request execution
-    this.loadingIndicatorService.onStarted(req);
-    
-    return next
-      .handle(req)
-      // emit onFinished event after request execution
-      .finally(() => this.loadingIndicatorService.onFinished(req));
-  }
-  
-}
-
-@Injectable()
-export class LoadingIndicatorService {
-
-    private _loading: boolean = false;
-
-    get loading(): boolean {
-        return this._loading;
-    }
-
-    onRequestStarted(): void {
-        this._loading = true;
-    }
-
-    onRequestFinished(): void {
-        this._loading = false;
-    }
-}
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
