28da7113041aa9b3c7a53b7a5f415879674b3f6a
Björn Asplund <bjorn.asplund@norwegian.com>
2018-08-16 23:29:37 +0200

    config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  wpBaseUrl: "https://maridalenbrenneri.no/wp-json/"
+  wpBaseUrl: 'https://maridalenbrenneri.no/wp-json/',
+  mbApiBaseUrl: 'https://maridalenbrenneri-api.herokuapp.com/api/'
 };
