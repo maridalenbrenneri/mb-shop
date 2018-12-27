1c08c2426d681213d70f9b008af7217176995920
Björn <bjuffe@gmail.com>
2018-12-27 08:32:17 +0100

    Work

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   wpBaseUrl: 'https://maridalenbrenneri.no/wp-json/',
-  mbApiBaseUrl: 'https://maridalenbrenneri-backend.herokuapp.com/api/'
+  mbApiBaseUrl: 'https://maridalenbrenneri-api.herokuapp.com/api/'
 };
