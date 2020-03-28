100be572daf8183ff5e248c4579b40b2ba053f56
bjoda <bjuffe@gmail.com>
2020-03-28 09:30:09 +0100

    work

--- client/src/app/shop/components/my-account/my-account.component.ts
+++ client/src/app/shop/components/my-account/my-account.component.ts
@@ -1,18 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthService } from '../../services/auth.service';
-import { User } from '../../models/user.model';
+import { Component, OnInit } from "@angular/core";
+import { AuthService } from "../../services/auth.service";
+import { User } from "../../models/user.model";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-my-account',
-  templateUrl: './my-account.component.html',
-  styleUrls: ['./my-account.component.scss']
+  selector: "app-my-account",
+  templateUrl: "./my-account.component.html",
+  styleUrls: ["./my-account.component.scss"]
 })
 export class MyAccountComponent implements OnInit {
-
   user: User;
   tested: string;
 
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService, private router: Router) {
     this.user = new User();
   }
 
@@ -25,8 +25,9 @@ export class MyAccountComponent implements OnInit {
   }
 
   onSignedIn(signedIn: boolean) {
-    if(signedIn) {
+    if (signedIn) {
       this.getUser();
+      this.router.navigate(["/dashboard"]);
     }
   }
 
