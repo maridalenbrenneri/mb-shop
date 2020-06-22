ea22f52eef3b103c78a47eadc17492895e608d7c
bjoda <bjuffe@gmail.com>
2020-06-22 19:19:46 +0200

    work

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ develop
 
 ./server - npm run dev
 ./client - ng serve
+
+## jobs
+
+npm run add-delivery-days
--- server/src/jobs/add-delivery-days.ts
+++ server/src/jobs/add-delivery-days.ts
@@ -3,15 +3,19 @@ require("dotenv").config();
 import DeliveryDayModel from "../database/models/delivery-day-model";
 
 const DELIVERY_DAYS = [
-  { date: new Date("2020-05-11"), type: "normal" },
-  { date: new Date("2020-05-18"), type: "fortnightly" },
-  { date: new Date("2020-05-25"), type: "normal" },
-  { date: new Date("2020-06-01"), type: "monthly" },
-  { date: new Date("2020-06-08"), type: "normal" },
-  { date: new Date("2020-06-15"), type: "fortnightly" },
-  { date: new Date("2020-05-22"), type: "normal" },
-  { date: new Date("2020-06-29"), type: "normal" },
-  { date: new Date("2020-07-06"), type: "monthly" },
+  { date: new Date("2020-07-13"), type: "normal" },
+  { date: new Date("2020-07-20"), type: "fortnightly" },
+  { date: new Date("2020-07-27"), type: "normal" },
+  { date: new Date("2020-08-03"), type: "monthly" },
+  { date: new Date("2020-08-10"), type: "normal" },
+  { date: new Date("2020-08-17"), type: "fortnightly" },
+  { date: new Date("2020-08-24"), type: "normal" },
+  { date: new Date("2020-08-31"), type: "monthly" },
+  { date: new Date("2020-09-07"), type: "normal" },
+  { date: new Date("2020-09-14"), type: "fortnightly" },
+  { date: new Date("2020-09-21"), type: "normal" },
+  { date: new Date("2020-09-28"), type: "normal" },
+  { date: new Date("2020-10-05"), type: "monthly" },
 ];
 
 const add = () => {
